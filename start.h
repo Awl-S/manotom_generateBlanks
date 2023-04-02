9d6af0869c03133f06b462dca3aa8ce9c71d206e
orys <batnikov@internet.ru>
2023-04-02 23:25:57 +0700

    Оптимизация

--- start.h
+++ start.h
@@ -1,7 +1,3 @@
-//
-// Created by orys on 02.04.23.
-//
-
 #ifndef MANOTOM_GENERATEBLANKS_START_H
 #define MANOTOM_GENERATEBLANKS_START_H
 
@@ -15,78 +11,44 @@
 #include "src/struct/settings/settings.h"
 #include "src/platform/Manager/Manager.h"
 
-void start(int argc, char *argv[]){
-    if(argc > 1){
-        //Конвертация
-        if(std::strcmp(argv[1], "-c") == 0){
+void start(int argc, char *argv[]) {
+    if (argc > 1) {
+        const char* cmd = argv[1];
+        // Конвертация
+        if (std::strcmp(cmd, "-c") == 0) {
             Converter::ConverterUTF(argv[2], argv[3]);
-        }
-        if(std::strcmp(argv[1], "-C") == 0){
+        } else if (std::strcmp(cmd, "-C") == 0) {
             Converter::ConverterUTF(argv[2], argv[3], argv[4], argv[5]);
-        }
-        if(std::strcmp(argv[1], "--c") == 0){
+        } else if (std::strcmp(cmd, "--c") == 0) {
             Converter::convert_file("WINDOWS-1251", "UTF-8", argv[2], argv[3]);
         }
-        //Настройка путей
-        if(std::strcmp(argv[1], "-new") == 0){
-            if(argc == 2){
-                PathMaker::create(); // без указания пути
-            }
-            else{
-                PathMaker::create(argv[2]); // c указанием пути
-            }
-        }
-        if(std::strcmp(argv[1], "-edit") == 0){
-            if(std::strcmp(argv[2], "-path")==0){
-                for (int i = 3; i < argc; ++i) {
-                    PathMaker::editJsonFile( argv[i]); // c указанием пути
-                }
-            } else{
-                for (int i = 2; i < argc; ++i) {
-                    PathMaker::editJsonFile(argv[2], argv[i]); // c указанием пути
-                }
+            // Настройка путей
+        else if (std::strcmp(cmd, "-new") == 0) {
+            const char* path = (argc == 2) ? "" : argv[2];
+            PathMaker::create(path);
+        } else if (std::strcmp(cmd, "-edit") == 0) {
+            const char* path_arg = argv[2];
+            for (int i = 3; i < argc; ++i) {
+                PathMaker::editJsonFile((std::strcmp(path_arg, "-path") == 0) ? argv[i] : path_arg, argv[i]);
             }
         }
-
-        if(std::strcmp(argv[1], "-печать") == 0 || std::strcmp(argv[1], "-print") == 0){
-            settings* data = new settings("", 0, 0, 0);
-            data->accuracy = argv[2];
-            data->color = atof(argv[3]);
-
-            // Температура
-            if(std::strcmp(argv[4], "true") == 0 || std::strcmp(argv[4], "1") == 0){
-                data->temperature = true;
-            }
-            if(std::strcmp(argv[4], "false") == 0 || std::strcmp(argv[4], "0") == 0){
-                data->temperature = false;
-            }
-
-            // Контур
-            if(std::strcmp(argv[5], "true") == 0 || std::strcmp(argv[5], "1") == 0){
-                data->contour = true;
-            }
-            if(std::strcmp(argv[5], "false") == 0 || std::strcmp(argv[5], "0") == 0){
-                data->contour = false;
-            }
-            //путь, класс точности, bool -65, double значение цвета, bool контур
+            // Печать
+        else if (std::strcmp(cmd, "-печать") == 0 || std::strcmp(cmd, "-print") == 0) {
+            settings data("", std::stoi(argv[2]), std::atof(argv[3]), std::strcmp(argv[4], "true") == 0 || std::strcmp(argv[4], "1") == 0);
+            data.contour = std::strcmp(argv[5], "true") == 0 || std::strcmp(argv[5], "1") == 0;
+            // путь, класс точности, bool -65, double значение цвета, bool контур
         }
-
-        if(std::strcmp(argv[1], "-help") == 0) {
-            std::string line;
+            // Справка
+        else if (std::strcmp(cmd, "-help") == 0) {
             std::ifstream file("help.txt");
-            while(std::getline(file, line)){
-                std::cout << line << std::endl;
-            }
+            std::cout << file.rdbuf();
             file.close();
         }
-    }
-    else{
+    } else {
         auto data = PathMaker::getDataStruct("/home/orys/CLionProjects/manotom_generateBlanks/cmake-build-debug/path.json");
         auto [dataVector, dataCFM, dataZGT, dataNBR] =  Manager::readFile(data, "/home/orys/Desktop/target/манометры/p4-6/");
-
-        for (auto & i : dataVector) {
-            std::cout << i->position << std::endl;
-            std::cout << i->model << std::endl;
+        for (auto& i : dataVector) {
+            std::cout << i->position << std::endl << i->model << std::endl;
         }
     }
 }
